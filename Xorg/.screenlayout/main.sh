cd7259f1028b23acb62194af6e2ec4e807e4b64d
Alexey Grant <aleksey@chabunin.com>
2020-05-11 22:09:08 +1000

    Changed Xorg configuration

--- Xorg/.screenlayout/main.sh
+++ Xorg/.screenlayout/main.sh
@@ -1,10 +1,2 @@
-
-# Set refresrate to 120Hz
+#!/bin/sh
 xrandr --output DVI-D-0 --off --output HDMI-0 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-0 --off --output DP-1 --off --output DP-2 --mode 3440x1440 --rate 120 --pos 2560x0 --rotate normal --output DP-3 --off
-~/.scre
-
-# Background Image for spectrwm
-feh --bg-fill ~/Tools/Backgrounds/milky-way-2695569_1920.jpg
-
-# Sound Volume
-amixer set Master 75%
